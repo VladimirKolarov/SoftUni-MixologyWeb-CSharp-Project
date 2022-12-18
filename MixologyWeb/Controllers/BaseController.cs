428db059719773070b64806b019fc13ecd8150c2
VladimirKolarov <Vladimir.Kolarov@yahoo.com>
2022-12-18 23:20:51 +0200

    Identity Scaffolding

---
+++ MixologyWeb/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MixologyWeb.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}

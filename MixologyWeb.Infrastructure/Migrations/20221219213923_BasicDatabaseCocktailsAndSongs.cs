454ad42502e9f331f616539bf303a5bee211dce8
VladimirKolarov <Vladimir.Kolarov@yahoo.com>
2022-12-19 23:47:34 +0200

    Basic Database Migration

---
+++ MixologyWeb.Infrastructure/Migrations/20221219213923_BasicDatabaseCocktailsAndSongs.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MixologyWeb.Infrastructure.Data.Migrations
+{
+    public partial class BasicDatabaseCocktailsAndSongs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cocktails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true),
+                    PreparationInfo = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cocktails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Measurements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Performers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Performers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ExternalLink = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CocktailId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Cocktails_CocktailId",
+                        column: x => x.CocktailId,
+                        principalTable: "Cocktails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    MeasurementId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Measurements_MeasurementId",
+                        column: x => x.MeasurementId,
+                        principalTable: "Measurements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CocktailSong",
+                columns: table => new
+                {
+                    CocktailsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SongsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CocktailSong", x => new { x.CocktailsId, x.SongsId });
+                    table.ForeignKey(
+                        name: "FK_CocktailSong_Cocktails_CocktailsId",
+                        column: x => x.CocktailsId,
+                        principalTable: "Cocktails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CocktailSong_Songs_SongsId",
+                        column: x => x.SongsId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PerformerSong",
+                columns: table => new
+                {
+                    PerformersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SongsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PerformerSong", x => new { x.PerformersId, x.SongsId });
+                    table.ForeignKey(
+                        name: "FK_PerformerSong_Performers_PerformersId",
+                        column: x => x.PerformersId,
+                        principalTable: "Performers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PerformerSong_Songs_SongsId",
+                        column: x => x.SongsId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientQuantities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IngredientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CocktailId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientQuantities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngredientQuantities_Cocktails_CocktailId",
+                        column: x => x.CocktailId,
+                        principalTable: "Cocktails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientQuantities_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailSong_SongsId",
+                table: "CocktailSong",
+                column: "SongsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CocktailId",
+                table: "Comments",
+                column: "CocktailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientQuantities_CocktailId",
+                table: "IngredientQuantities",
+                column: "CocktailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientQuantities_IngredientId",
+                table: "IngredientQuantities",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_MeasurementId",
+                table: "Ingredients",
+                column: "MeasurementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerformerSong_SongsId",
+                table: "PerformerSong",
+                column: "SongsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CocktailSong");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "IngredientQuantities");
+
+            migrationBuilder.DropTable(
+                name: "PerformerSong");
+
+            migrationBuilder.DropTable(
+                name: "Cocktails");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Performers");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Measurements");
+        }
+    }
+}
---
+++ MixologyWeb.Infrastructure/obj/Debug/net6.0/qmbidxlv.sql
@@ -0,0 +1,110 @@
+﻿BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Cocktails] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(100) NOT NULL,
+    [ImageUrl] nvarchar(2048) NULL,
+    [PreparationInfo] nvarchar(1000) NOT NULL,
+    [Description] nvarchar(1000) NULL,
+    [Rating] float NULL,
+    CONSTRAINT [PK_Cocktails] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Measurements] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(20) NOT NULL,
+    CONSTRAINT [PK_Measurements] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Performers] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(100) NOT NULL,
+    CONSTRAINT [PK_Performers] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Songs] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(100) NOT NULL,
+    [ExternalLink] nvarchar(2048) NULL,
+    CONSTRAINT [PK_Songs] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Comments] (
+    [Id] uniqueidentifier NOT NULL,
+    [Text] nvarchar(1000) NOT NULL,
+    [CreatedOn] datetime2 NOT NULL,
+    [CocktailId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_Comments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Comments_Cocktails_CocktailId] FOREIGN KEY ([CocktailId]) REFERENCES [Cocktails] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Ingredients] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(50) NOT NULL,
+    [Description] nvarchar(500) NULL,
+    [MeasurementId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_Ingredients] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Ingredients_Measurements_MeasurementId] FOREIGN KEY ([MeasurementId]) REFERENCES [Measurements] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [CocktailSong] (
+    [CocktailsId] uniqueidentifier NOT NULL,
+    [SongsId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_CocktailSong] PRIMARY KEY ([CocktailsId], [SongsId]),
+    CONSTRAINT [FK_CocktailSong_Cocktails_CocktailsId] FOREIGN KEY ([CocktailsId]) REFERENCES [Cocktails] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_CocktailSong_Songs_SongsId] FOREIGN KEY ([SongsId]) REFERENCES [Songs] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [PerformerSong] (
+    [PerformersId] uniqueidentifier NOT NULL,
+    [SongsId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_PerformerSong] PRIMARY KEY ([PerformersId], [SongsId]),
+    CONSTRAINT [FK_PerformerSong_Performers_PerformersId] FOREIGN KEY ([PerformersId]) REFERENCES [Performers] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_PerformerSong_Songs_SongsId] FOREIGN KEY ([SongsId]) REFERENCES [Songs] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [IngredientQuantities] (
+    [Id] uniqueidentifier NOT NULL,
+    [IngredientId] uniqueidentifier NOT NULL,
+    [CocktailId] uniqueidentifier NOT NULL,
+    [Quantity] float NOT NULL,
+    CONSTRAINT [PK_IngredientQuantities] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_IngredientQuantities_Cocktails_CocktailId] FOREIGN KEY ([CocktailId]) REFERENCES [Cocktails] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_IngredientQuantities_Ingredients_IngredientId] FOREIGN KEY ([IngredientId]) REFERENCES [Ingredients] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_CocktailSong_SongsId] ON [CocktailSong] ([SongsId]);
+GO
+
+CREATE INDEX [IX_Comments_CocktailId] ON [Comments] ([CocktailId]);
+GO
+
+CREATE INDEX [IX_IngredientQuantities_CocktailId] ON [IngredientQuantities] ([CocktailId]);
+GO
+
+CREATE INDEX [IX_IngredientQuantities_IngredientId] ON [IngredientQuantities] ([IngredientId]);
+GO
+
+CREATE INDEX [IX_Ingredients_MeasurementId] ON [Ingredients] ([MeasurementId]);
+GO
+
+CREATE INDEX [IX_PerformerSong_SongsId] ON [PerformerSong] ([SongsId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20221219212530_BasicDatabaseCocktailsAndSongs', N'6.0.12');
+GO
+
+COMMIT;
+GO
+

5865cdc34e0bc96982059a129bf8dd5a3c262597
VladimirKolarov <Vladimir.Kolarov@yahoo.com>
2022-12-20 04:15:08 +0200

    Service Collection Extension

--- MixologyWeb/Program.cs
+++ MixologyWeb/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using MixologyWeb.Core.Constants;
 using MixologyWeb.Infrastructure.Data;
 using MixologyWeb.ModelBinders;
@@ -7,10 +6,7 @@ using MixologyWeb.ModelBinders;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddApplicationDbContexts(builder.Configuration);
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
@@ -21,6 +17,10 @@ builder.Services.AddControllersWithViews()
         options.ModelBinderProviders.Insert(1, new DoubleModelBinderProvider());
         options.ModelBinderProviders.Insert(2, new DateTimeModelBinderProvider(FormatingConstant.DateFormatBG));
     });
+
+builder.Services.AddApplicationServices();
+
+
 
 var app = builder.Build();
 
